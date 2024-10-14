3893732a4fb79e1bc56008b0fc89505ab47a83a9
MuammarRizal <muammarrizal31@gmail.com>
2024-10-14 22:14:23 +0700

    Setup project

---
+++ src/app/about/profile/navbar.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Navbar = () => {
+  return <div>Ini adalah Navbar</div>;
+};
+
+export default Navbar;
---
+++ src/app/about/profile/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "./navbar";
+
+const ProfilePage = () => {
+  return (
+    <div>
+      <Navbar />
+      ProfilePage
+    </div>
+  );
+};
+
+export default ProfilePage;
